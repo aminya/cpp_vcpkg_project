58c05a6b19d319d428d1fbb1f0701cf9de5ebd74
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-03-19 15:29:10 -0700

    feat: update the top CMake file for vcpkg and production code

---
+++ Features.cmake
@@ -0,0 +1,11 @@
+# building the tests
+option(FEATURE_TESTS "Enable the tests" OFF)
+if(FEATURE_TESTS)
+  list(APPEND VCPKG_MANIFEST_FEATURES "tests")
+endif()
+
+# building the docs
+option(FEATURE_DOCS "Enable the docs" OFF)
+
+# fuzz tests
+option(FEATURE_FUZZ_TESTS "Enable the fuzz tests" OFF)
